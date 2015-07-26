a5db2c53bee51f9c5bad2587ea258e95c5a5ec73
Leslie Kimaiyo Chemwolo <kimaiyoc@gmail.com>
2015-07-26 10:23:57 +0300

    Updated documentation

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ pageWithSidebar(
 # Application title
 headerPanel("Getting average of three numbers"),
 sidebarPanel(
-h5("Click here for help:", a("HELP", href="https://github.com/lchemwolo/avarager/blob/master/Averager%20Presentation.html")),
+h5("Click here for help:", a("HELP", href="http://rpubs.com/lchemwolo/averager")),
 numericInput('number1', 'First Number', 90, min = 50, max = 200, step = 5),
 numericInput('number2', 'Second Number', 90, min = 50, max = 200, step = 5),
 numericInput('number3', 'Third Number', 90, min = 50, max = 200, step = 5),
